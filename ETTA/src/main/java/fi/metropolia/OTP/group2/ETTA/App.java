312a4f786f0fa6041acdddbe09e705b2696f8c19
Essi Norri <essi.norri@outlook.com>
2020-01-20 10:49:57 +0200

    ensimmäinen kommitointi

---
+++ ETTA/src/main/java/fi/metropolia/OTP/group2/ETTA/App.java
@@ -0,0 +1,13 @@
+package fi.metropolia.OTP.group2.ETTA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
