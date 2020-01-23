eed9e99bffe599653bde5b9d349d701c5f4a17d2
Essi Norri <essi.norri@outlook.com>
2020-01-23 15:24:32 +0200

    Trying out

---
+++ ETTA/src/main/java/fi/metropolia/OTP/group2/ETTA/App1.java
@@ -0,0 +1,14 @@
+package fi.metropolia.OTP.group2.ETTA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!");
+    }
+}
+
