1d90d2435a2ecd2e789f9c8b02e4fb123609594c
Anni <annikark@metropolia.fi>
2020-02-03 10:25:14 +0200

    Tein luokkia talouteen

--- ETTA/src/main/java/model/All_savings.java
+++ ETTA/src/main/java/model/All_savings.java
@@ -1,5 +1,35 @@
 package model;
 
-public class All_savings {
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="All_savings")
+
+public class All_savings{
+	
+	@Id
+	@Column(name="saving_id")
+	private int saving_id;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="amount")
+	private float amount;
+	
+	@Column(name="goal_amount")
+	private float goal_amount;
+	
+	@Column(name="goal_date")
+	private Date goal_date;
+	
+	@Column(name="reached_goal")
+	private float reached_goal;
+	
 }
