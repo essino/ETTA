15a586e5fc118b03d3d40936cc2f810c84a4bb23
Elena Mylläri <elena.myllari@metropolia.fi>
2020-02-07 12:17:33 +0200

    javadoc comments

--- ETTA/src/main/java/model/Balance.java
+++ ETTA/src/main/java/model/Balance.java
@@ -2,6 +2,10 @@ package model;
 
 import javax.persistence.*;
 
+/**
+ * Model class for balance. Used in the reading and updating of the database table for Balance through Hibernate.
+ */
+
 @Entity
 @Table(name="Balance")
 public class Balance {
@@ -12,31 +16,56 @@ public class Balance {
 	@Column
 	private float balance = 0;
 	
+	/**
+	 * Constructor to create balance. No parameters.
+	 */
 	public Balance() {
 		
 	}
 
+	/**
+	 * Function to fetch the id number of the balance from the database.
+	 * @return id the id number of the balance in the database
+	 */
 	public int getId() {
 		return id;
 	}
-
+	/**
+	 * Function to register the id number of the balance into the database
+	 * @param id the id number of the balance in the database
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	/**
+	 * Function to fetch the amount of the balance from the database.
+	 * @return balance float amount of the balance
+	 */
 	public float getBalance() {
 		return balance;
 	}
-
+	/**
+	 * Function to register the amount of the balance into the database
+	 * @param balance float amount of the balance
+	 */
 	public void setBalance(float balance) {
 		this.balance = balance;
 	}
 
+	/**
+	 * Constructor to create balance.
+	 *@param balance float amount of the balance
+	 */
 	public Balance(float balance) {
 
 		this.balance = balance;
 	}
 	
+	/**
+	 * Constructor to create balance.
+	 *@param id the id number of the balance in the database
+	 *@param balance float amount of the balance
+	 */
 	public Balance(int id, float balance) {
 		this.id = id;
 		this.balance = balance;
