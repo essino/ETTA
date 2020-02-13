e42351cf8a4cdf36f4ffac0fcbcda4322b3c2e47
Anni <annikark@metropolia.fi>
2020-02-13 15:01:36 +0200

    Henkilöitä voi lisätä tietokantaan

--- ETTA/src/main/java/controller/ContactsController.java
+++ ETTA/src/main/java/controller/ContactsController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.Date;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,7 +9,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import model.Person;
+import model.PersonDAO;
+import view.ContactsGUI;
 
 public class ContactsController {
+	
+	private ContactsGUI conGUI;
+	private PersonDAO perDAO = new PersonDAO();
+	
+	
+	public void savePerson() {
+		String PersonName = conGUI.getPersonName();
+		String PersonAddress = conGUI.getPersonAddress();
+		String PersonEmail = conGUI.getPersonEmail();
+		Date PersonBirthday = conGUI.getPersonBirthday();
+		
+		Person person = new Person (PersonName, PersonBirthday, PersonEmail);
+		
+		Boolean Person = perDAO.createPerson(person);
+	}
+	
+	public ContactsController(ContactsGUI conGUI) {
+		this.conGUI = conGUI;
+	}
 	
 }
