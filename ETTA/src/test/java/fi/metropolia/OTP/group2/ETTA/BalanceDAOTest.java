3287b981df16176b2814437129c8586fda2b6a4b
Elena Mylläri <elena.myllari@metropolia.fi>
2020-02-27 20:33:00 +0200

    test

--- ETTA/src/test/java/fi/metropolia/OTP/group2/ETTA/BalanceDAOTest.java
+++ ETTA/src/test/java/fi/metropolia/OTP/group2/ETTA/BalanceDAOTest.java
@@ -36,9 +36,9 @@ public class BalanceDAOTest {
 	@Order(2)
 	public void testUpdate() {
 		Balance balance = balanceDAO.readBalance(id);
-		balance.setBalance(10);
+		balance.setBalance(0);
 		balanceDAO.updateBalance(balance);
-		assertEquals(10, balanceDAO.readBalance(id).getBalance(), "balance amount updating failed");
+		assertEquals(0, balanceDAO.readBalance(id).getBalance(), "balance amount updating failed");
 		
 	}
 	
