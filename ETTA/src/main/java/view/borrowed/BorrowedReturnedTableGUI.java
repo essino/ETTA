584b4af7cf031f39012b77bc65194c4635c96ed5
Essi Norri <essi.norri@outlook.com>
2020-04-13 11:19:49 +0300

    borrowed local...

--- ETTA/src/main/java/view/borrowed/BorrowedReturnedTableGUI.java
+++ ETTA/src/main/java/view/borrowed/BorrowedReturnedTableGUI.java
@@ -110,7 +110,7 @@ public class BorrowedReturnedTableGUI {
 			returned.setCellValueFactory(new Callback<CellDataFeatures<BorrowedThing, String>, ObservableValue<String>>(){
 				public ObservableValue<String> call(CellDataFeatures<BorrowedThing, String> borrowedThingDescr) {
 					if (borrowedThingDescr.getValue().isReturned() == true) {
-						//return new ReadOnlyObjectWrapper<>(myBundle.getBundle().getString("yesYes"));
+						//return new ReadOnlyObjectWrapper<>(myBundle.getBundle().getString("yesYes")); 
 						return new ReadOnlyObjectWrapper<>("Yes");
 					} else {
 						//return new ReadOnlyObjectWrapper<>(myBundle.getBundle().getString("noNo"));
