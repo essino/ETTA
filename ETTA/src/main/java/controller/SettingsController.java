a0704b0afd6f999fb9a73f91347c99b8d0b77962
Elena Mylläri <elena.myllari@metropolia.fi>
2020-05-03 10:07:36 +0300

    comments

--- ETTA/src/main/java/controller/SettingsController.java
+++ ETTA/src/main/java/controller/SettingsController.java
@@ -13,25 +13,54 @@ import model.Language;
 import model.LanguageDAO;
 import res.MyTab;
 
+/** 
+ * A singleton controller class for the language settings. 
+ * @author Lena
+ */
 public class SettingsController {
+	/**
+	 * the settingsController instance
+	 */
 	public static final SettingsController single = new SettingsController();
 
+	/**
+	 * LanguageDAO used for accessing the database
+	 */
 	private LanguageDAO langDAO = new LanguageDAO();
 
-	private Locale locale = new Locale("en", "GB");
+	/**
+	 * Reference to the Locale
+	 */
 	private Locale currentLocale;
 	
+	/**
+	 * Language object for adding English language to the database. English is the default language, so chosen is set to true
+	 */
 	Language english = new Language(1,"English", true);
+	/**
+	 * Language object for adding Finnish language to the database. English is the default language, so chosen is set to false
+	 */
 	Language finnish = new Language(2, "Finnish", false);
 
+	/**
+	 * Constructor. The current Locale is decided depending on the current chosen language
+	 */
 	private SettingsController() {
 		this.currentLocale= getLocaleFromDatabase();
 	}
 	
+	/**
+	 * method for getting the instance of the SettingsController
+	 * @return SettingsController the instance of the SettingsController
+	 */
 	public static SettingsController getInstance() {
 		return single;
 	}
 	
+	/**
+	 * Method for getting the current Locale depending on the chosen language got from the database.
+	 * @return Locale the current locale depending on the chosen language
+	 */
 	private Locale getLocaleFromDatabase() {
 		if(getSelectedLanguage().equals("Finnish")) {
 			return new Locale("fi", "FI");
@@ -41,10 +70,18 @@ public class SettingsController {
 		}
 	}
 	
+	/**
+	 * Method for getting the current Locale
+	 * @return Locale the current Locale
+	 */
 	public Locale getCurrentLocale() {
 		return this.currentLocale;
 	}
 
+	/**
+	 * Method for fetching all languages from the database
+	 * @return ObservableList<String> list containing the names of the languages from the database
+	 */
 	public ObservableList<String> languageList() {
 		Language[] langs = langDAO.readLanguages();
 		ArrayList<String> langNames = new ArrayList();
@@ -94,6 +131,11 @@ public class SettingsController {
 		}
 	}
 
+	/**
+	 * Method for fetching the selected language from the database. 
+	 * If there are no languages in the database yet, a method adding languages to the databased is called.
+	 * @return String the name of the chosen language
+	 */
 	public String getSelectedLanguage() {
 		Language selectedLanguage = langDAO.getSelectedLanguage();
 		//none of the languages is selected
@@ -108,6 +150,10 @@ public class SettingsController {
 		return langDAO.getSelectedLanguage().getDescription();
 	}
 	
+	/** 
+	 * Method that asks LanguageDAO to add Finnish and English language to the database. 
+	 * The method is called if there are no languages in the database yet. 
+	 */
 	private void createLanguages() {
 		langDAO.createLanguage(english);
 		langDAO.createLanguage(finnish);
